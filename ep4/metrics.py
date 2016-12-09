1beb843e964687e12f50a881a43d0a758846c753
Yara Grassi Gouffon <vaati2005-git@yahoo.com.br>
2016-12-09 01:37:13 -0500

    Metrics implemented

--- ep4/metrics.py
+++ ep4/metrics.py
@@ -4,12 +4,32 @@ import util
 
 def gini( labels ) :
 	"*** YOUR CODE HERE ***"
-	util.raiseNotDefined()
+	# util.raiseNotDefined()
+	# print labels
+	counter = util.Counter()
+	counter.incrementAll( labels , 1 )
+	counter.normalize()
+	res = 1
+	for key in counter:
+		res -= counter[key]*counter[key]
+	return res
 
 def error( labels ) :
 	"*** YOUR CODE HERE ***"
-	util.raiseNotDefined()
+	# util.raiseNotDefined()
+	counter = util.Counter()
+	counter.incrementAll( labels , 1 )
+	counter.normalize()
+	res = 1 - counter[counter.argMax()]
+	return res
 
 def entropy( labels ) :
 	"*** YOUR CODE HERE ***"
-	util.raiseNotDefined()
+	# util.raiseNotDefined()
+	counter = util.Counter()
+	counter.incrementAll( labels , 1 )
+	counter.normalize()
+	res = 0
+	for key in counter:
+		res -= counter[key]*math.log(counter[key])
+	return res
